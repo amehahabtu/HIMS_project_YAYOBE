a2e5feef6d547c383403a1403bf15d11c0c241e5
Ameha <69168737+amehahabtu@users.noreply.github.com>
2020-10-14 02:30:23 -0700

    This test github application on SQL db project

---
+++ HIMS_database/patient.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[patient]
+(
+	[MedRecNo] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Address] NCHAR(10) NULL, 
+    [Gender] NCHAR(10) NULL
+)
